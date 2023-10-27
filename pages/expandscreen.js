5d5ca25657d42df7cd7d144e7f8247be5afcbfb1
Gokul-pga <gokulanand2508@gmail.com>
2023-10-27 12:50:22 +0530

    ...

--- pages/expandscreen.js
+++ pages/expandscreen.js
@@ -1,6 +1,15 @@
-// export const expandMyScreen = () => {
-//   const element = document.documentElement;
-//   if (element.requestFullscreen) {
-//     element.requestFullscreen();
-//   }
-// };
+// import React from 'react'
+
+// function expandscreen() {
+//   return (
+//     <div>expandscreen</div>
+//   )
+// }
+// // export const expandMyScreen = () => {
+// //   const element = document.documentElement;
+// //   if (element.requestFullscreen) {
+// //     element.requestFullscreen();
+// //   }
+// // };
+
+// export default expandscreen
